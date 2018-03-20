059f10ab51bf140c12a20cd42303410aecf8e033
Johannes Wachter <johannes.wachter@sulu.io>
2018-03-20 14:00:38 +0100

    extended order entity by gift-wrapping field

---
+++ src/Entity/Order.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Acme\SyliusExamplePlugin\Entity;
+
+class Order extends \Sylius\Component\Core\Model\Order
+{
+    /**
+     * @var bool
+     */
+    private $giftWrapping;
+
+    public function isGiftWrapping(): bool
+    {
+        return $this->giftWrapping;
+    }
+
+    public function setGiftWrapping(bool $giftWrapping): void
+    {
+        $this->giftWrapping = $giftWrapping;
+    }
+}
